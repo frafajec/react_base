afc8039ba5cbbcfbf7e6f37450fe48ef58ade32a
Filip Rafajec <filip.rafajec@gmail.com>
2018-09-21 19:05:51 +0200

    Folder structure

--- src/modules/App/App.tsx
+++ src/modules/App/App.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as cn from 'classnames';
-import { Theme, withStyles, WithStyles } from '@material-ui/core/styles';
 
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
@@ -10,31 +9,17 @@ import Logo from 'assets/icons/logo.svg';
 import styles from './App.scssm';
 
 // ------------------------------------------------------------
-const material: any = (theme: Theme) => ({
-  paper: {
-    color: theme.palette.text.secondary,
-    padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-  },
-  root: {
-    flexGrow: 1,
-  },
-});
-
-// ------------------------------------------------------------
-interface IAppProps extends WithStyles<typeof material> {}
+interface IAppProps {}
 interface IAppState {}
 
 // ------------------------------------------------------------
 class App extends React.Component<IAppProps, IAppState> {
   render() {
-    const { classes } = this.props;
-
     return (
-      <div className={cn(classes.root, styles.App)}>
+      <div className={cn(styles.App)}>
         <Grid container spacing={24}>
           <Grid item xs>
-            <Paper className={classes.paper}>xs</Paper>
+            <Paper className={styles.paper}>xs</Paper>
           </Grid>
           <Grid item xs>
             <Logo className={styles.AppLogo} />
@@ -46,4 +31,4 @@ class App extends React.Component<IAppProps, IAppState> {
 }
 
 // ------------------------------------------------------------
-export default withStyles(material)(App);
+export default App;
